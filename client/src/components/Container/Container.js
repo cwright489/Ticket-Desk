c2e455dd0c69b4a25ead980623cf6ca7a76fd719
Tamara <tamara@Es-MacBook-Pro.local>
2020-05-21 19:02:05 -0600

    added styling

--- client/src/components/Container/Container.js
+++ client/src/components/Container/Container.js
@@ -1,26 +1,56 @@
 import React, { Component , useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        
     },
     paper: {
         padding: theme.spacing(1),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        
+        
     },
+    table: {
+        minWidth: 400,
+        // maxWidth: "725px",
+      },
 }));
 
-
-
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+  
+  const StyledTableRow = withStyles((theme) => ({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }))(TableRow);
+  
 
 export default function Container() {
     const classes = useStyles();
 
-   const [tickets, setTickets] = useState("")
+   const [tickets, setTickets] = useState({tix:[]})
 
    useEffect(() =>
     {
@@ -32,90 +62,36 @@ export default function Container() {
    
         })
 
-        const getTix = () => {
-            return tickets.tix.map(ticket => {
-            return <div>
-                <h1>{ticket.userAssigned}</h1>
-                <h2>{ticket.issueDesc}</h2>
-                <h2>{ticket.status}</h2>
-                <h2>{ticket.priority}</h2>
-                </div>
-            })
-        }
+    
 
-    function FormRow() {
-        return (
-            <React.Fragment>
-              
-        
-            
-            </React.Fragment>
-        );
-    } 
-    
-    return (
-    <>
-    <h1>HELLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLo</h1>
-     <div style={{textAlign: "center"}}>
-                {tickets.tix
-                 ? 
-                <h1>{getTix()}</h1>
-                    // <Grid item xs={4}>
-                    //     <Paper className={classes.paper}>STATUS</Paper>
-                    // </Grid>
-                    // <Grid item xs={4}>
-                    //     <Paper className={classes.paper}>DEPARTMENT</Paper>
-                    // </Grid>
-                    : <h1>Nope </h1>
-            }
-                </div>
-    </>
-        // <div className={classes.root}>
-        //     <Grid container spacing={1}>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //         <Grid container item xs={12} spacing={3}>
-        //             <FormRow />
-        //         </Grid>
-        //     </Grid>
-        // </div >
-    );
-    
+
+  return (
+    <TableContainer id = "tableContainer" component={Paper}className = {classes.paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align="left">Name</StyledTableCell>
+            <StyledTableCell align="left">Description</StyledTableCell>
+            <StyledTableCell align="center">Status</StyledTableCell>
+            <StyledTableCell align="right">Priority</StyledTableCell>
+            <StyledTableCell align="right">Take Ownership</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tickets.tix.map((ticket,i) => (
+            <StyledTableRow key={i}>
+              <StyledTableCell align="left">{ticket.userAssigned}</StyledTableCell>
+              <StyledTableCell align="left">{ticket.issueDesc}</StyledTableCell>
+              <StyledTableCell align="center">{ticket.status}</StyledTableCell>
+              <StyledTableCell align="right">{ticket.priority}</StyledTableCell>
+              <StyledTableCell align="right"> <Button variant="contained" color="primary">
+        Assign Ticket
+      </Button></StyledTableCell>
+
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
