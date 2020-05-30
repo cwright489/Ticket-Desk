887f3d7a04520df608b9d2a7f280379606823cb8
cwright489 <57241137+cwright489@users.noreply.github.com>
2020-05-30 13:53:14 -0600

    Revert "Alt passport"

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const session = require('express-session');
-const passport = require("./passport");
 
-
-//Express
 require('dotenv').config();
 
 const app = express();
@@ -14,32 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Passport
-app.use(passport.initialize())
-app.use(passport.session()) // calls serializeUser and deserializeUser
-
-// express sessions
-app.use(
-    session({
-        secret: 'fraggle-rock', //pick a random string to make the hash that is generated secure
-        resave: false, //required
-        saveUninitialized: false //required
-    })
-);
-
-app.use((req, res, next) => {
-    console.log('req.session', req.session);
-    return next();
-});
-
-app.post('/user', (req, res) => {
-    console.log('user signup');
-    req.session.username = req.body.username;
-    res.end()
-  })
-
-
-//mongoDB
 const uri = process.env.ATLAS_URI;
 
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
@@ -52,11 +22,9 @@ connection.once('open', () => {
 
 const ticketsRouter = require("./routes/tickets");
 const usersRouter = require("./routes/users");
-const signUpRouter = require("./routes/signUp");
 
 app.use('/tickets', ticketsRouter);
 app.use('/users', usersRouter);
-app.use('/signup', signUpRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port} !!`);
